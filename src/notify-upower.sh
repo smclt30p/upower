94b2406a3f25e279ac849d2d722cdcb0f1358a16
Richard Hughes <richard@hughsie.com>
2012-07-11 16:09:13 +0100

    fixup

--- src/notify-upower.sh
+++ src/notify-upower.sh
@@ -1,7 +1,6 @@
 ! /bin/sh
-[ "$1" = "post" ] && exec /usr/bin/dbus-send    \
-	--system --type signal		  \
-	--dest=org.freedesktop.UPower	   \
-	/org/freedesktop/UPower		 \
+[ "$1" = "post" ] && exec /usr/bin/dbus-send	\
+	--system --type=signal			\
+	--dest=org.freedesktop.UPower		\
+	/org/freedesktop/UPower			\
 	org.freedesktop.UPower.Resuming
-
